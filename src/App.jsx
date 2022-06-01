8806d21d9c549d3596828a655334c9530ea3e2a2
ANGELALIZARAZO <angela.maria.lizarazo.rueda@gmail.com>
2022-06-01 18:17:47 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React, { Component } from 'react'
-import Button from './Button'
+import React, { Component } from "react";
+import Button from "./Button";
 import "./App.css";
 
 export default class App extends Component {
-  colors = ["RED","BLUE","YELLOW", "GREEN","ORANGE","MAGENTA","BROWN","LIME"]
+  colors = [
+    "RED",
+    "BLUE",
+    "YELLOW",
+    "GREEN",
+    "ORANGE",
+    "MAGENTA",
+    "BROWN",
+    "LIME",
+  ]
+
   /* ESPACIO DE TRABAJO
   - Armar la estructura para una Class Component
   - Hacer un state con el arreglo de los colores
@@ -11,23 +21,48 @@ export default class App extends Component {
     formas tenemos para no perder el contexto?
   */
 
+  constructor() {
+    super();
+
+    this.state = {
+      colors: [
+        "RED",
+        "BLUE",
+        "YELLOW",
+        "GREEN",
+        "ORANGE",
+        "MAGENTA",
+        "BROWN",
+        "LIME",
+      ],
+    };
+   // this.shuffle = this.shuffle.bind(this);
+  
+  }
 
   /* ESPACIO DE TRABAJO
   Funcion Shuffle
   Aqui deberan hacer una funcion que me retorne un nuevo arreglo con los
   colores mezclados
   */
+schuffle=()=>{
 
-  
+  const arrayDesordenado = this.state.colors.sort (( ) => 0.5- Math.random());
+  this.setState({ colors: arrayDesordenado});
+}
+
   render() {
     return (
       <div className="container">
         <div className="panel">
-          {this.colors.map(color => <div key={color} className={color}>{color}</div>)}
+          {this.state.colors.map((color) => (
+            <div key={color} className={color}>
+              {color}
+            </div>
+          ))}
         </div>
-        <Button fn={"Recibe una funcion"} />
+        <Button fn={this.schuffle} />
       </div>
-
-    )
+    );
   }
 }
